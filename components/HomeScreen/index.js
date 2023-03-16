68f7246e6647595860d191c625c809fbd459f160
Keith Ma <95742118+imkeithyeet@users.noreply.github.com>
2023-03-15 18:36:13 -0700

    logo

--- components/HomeScreen/index.js
+++ components/HomeScreen/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 const HomeScreen = () => {
     return (
-        <div className='h-screen'>
+        <div className='h-screen overflow-y scroll snap-y snap-mandatory scroll-smooth'>
         <div
         style={{backgroundImage: 
             'url("https://tesla-cdn.thron.com/delivery/public/image/tesla/a269d7b4-87a9-4057-9ac5-2565e56a188e/bvlatuR/std/4096x2560/Homepage-Model-3-Desktop-LHD?quality=auto-medium&amp;format=auto")'
          }}
-         className='flex flex-col h-screen item-center justify-between bg-center bg-cover '
+         className='flex flex-col h-screen item-center justify-between bg-center bg-cover snap-start snap-always'
         >
             <div className='flex flex-col items-center justify-between h-full font-montserrat'>
             <div className='pt-48 text-center'>
@@ -31,7 +31,7 @@ const HomeScreen = () => {
         style={{backgroundImage: 
             'url("https://tesla-cdn.thron.com/delivery/public/image/tesla/81d6bbcd-e260-4176-86c4-cbd5033f9431/bvlatuR/std/1200x628/Model-Y-Social-EMEA-LHD?quality=auto-medium&format=auto")'
          }}
-         className='flex flex-col h-screen item-center justify-between bg-center bg-cover font-montserrat'
+         className='flex flex-col h-screen items-center justify-between bg-center bg-cover font-montserrat snap-start snap-always'
         >
             <div className='flex flex-col items-center justify-between h-full'>
             <div className='pt-48 text-center'>
@@ -53,7 +53,7 @@ const HomeScreen = () => {
         style={{backgroundImage: 
             'url("https://tesla-cdn.thron.com/delivery/public/image/tesla/58949946-f4bf-4908-b707-18b2fa6020a5/bvlatuR/std/2880x1800/Homepage-Model-S-Desktop-LHD?quality=auto-medium&amp;format=auto")'
          }}
-         className='flex flex-col h-screen item-center justify-between bg-center bg-cover font-montserrat'
+         className='flex flex-col h-screen item-center justify-between bg-center bg-cover font-montserrat snap-start snap-always'
         >
             <div className='flex flex-col items-center justify-between h-full'>
             <div className='pt-48 text-center'>
@@ -76,7 +76,7 @@ const HomeScreen = () => {
         style={{backgroundImage: 
             'url("https://tesla-cdn.thron.com/delivery/public/image/tesla/a4fd80a1-095e-4ce1-8db1-95b3ff880c26/bvlatuR/std/2880x1800/Model-X-Main-Hero-Desktop-RHD?quality=auto-medium&amp;format=auto%C2%A0")'
          }}
-         className='flex flex-col h-screen item-center justify-between bg-center bg-cover font-montserrat'
+         className='flex flex-col h-screen item-center justify-between bg-center bg-cover font-montserrat snap-start snap-always'
         >
             <div className='flex flex-col items-center justify-between h-full'>
             <div className='pt-48 text-center'>
@@ -99,7 +99,7 @@ const HomeScreen = () => {
         style={{backgroundImage: 
             'url("https://tesla-cdn.thron.com/delivery/public/image/tesla/45992f1c-a33a-4a04-b1f0-338aff182f8e/bvlatuR/std/2880x1800/_25-Hero-D")'
          }}
-         className='flex flex-col h-screen item-center justify-between bg-center bg-cover font-montserrat'
+         className='flex flex-col h-screen item-center justify-between bg-center bg-cover font-montserrat snap-start snap-always'
         >
             <div className='flex flex-col items-center justify-between h-full'>
             <div className='pt-48 text-center'>
@@ -121,7 +121,7 @@ const HomeScreen = () => {
         style={{backgroundImage: 
             'url("https://tesla-cdn.thron.com/delivery/public/image/tesla/5299492b-ead4-4a79-a96e-02a030c0bbc4/bvlatuR/std/0x0/tesla_solar_roof")'
          }}
-         className='flex flex-col h-screen item-center justify-between bg-center bg-cover font-montserrat'
+         className='flex flex-col h-screen item-center justify-between bg-center bg-cover font-montserrat snap-start snap-always'
         >
             <div className='flex flex-col items-center justify-between h-full'>
             <div className='pt-48 text-center'>
@@ -143,7 +143,7 @@ const HomeScreen = () => {
         style={{backgroundImage: 
             'url("https://www.teslarati.com/wp-content/uploads/2022/09/tesla-destination-charging-1-scaled.jpg")'
          }}
-         className='flex flex-col h-screen item-center justify-between bg-center bg-cover font-montserrat'
+         className='flex flex-col h-screen item-center justify-between bg-center bg-cover font-montserrat snap-start snap-always'
         >
             <div className='flex flex-col items-center justify-between h-full'>
             <div className='pt-48 text-center'>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,10 @@
-import HomeScreen from "../../components"
+import HomeScreen from "../../components/HomeScreen"
+import Header from "../../components/HomeScreen/Header"
+
 export default function Home() {
   return (
     <div className=''>
+      <Header/>
       <HomeScreen/>
     </div>
   )
