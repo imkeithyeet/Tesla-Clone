b0d68ce58dde4ccc91bba41c49dbb73464fe7f3c
Keith Ma <95742118+imkeithyeet@users.noreply.github.com>
2023-03-15 22:08:41 -0700

    head

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,16 @@
-import HomeScreen from "../../components/HomeScreen"
-import Header from "../../components/HomeScreen/Header"
+import Head from 'next/head';
+import HomeScreen from '../../components/HomeScreen';
+import Header from '../../components/HomeScreen/Header';
 
 export default function Home() {
   return (
     <div className=''>
+      <Head>
+        <title>Tesla Website Clone</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Header/>
       <HomeScreen/>
     </div>
-  )
+  );
 }
