4ffdddb867df05c0f552426749bb5ca0d57172d0
Keith Ma <95742118+imkeithyeet@users.noreply.github.com>
2023-03-15 17:53:06 -0700

    images and font

--- components/index.js
+++ components/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 const HomeScreen = () => {
     return (
-        <div className='h-screen border-2 border-red-400'>
+        <div className='h-screen'>
         <div
         style={{backgroundImage: 
             'url("https://tesla-cdn.thron.com/delivery/public/image/tesla/a269d7b4-87a9-4057-9ac5-2565e56a188e/bvlatuR/std/4096x2560/Homepage-Model-3-Desktop-LHD?quality=auto-medium&amp;format=auto")'
          }}
          className='flex flex-col h-screen item-center justify-between bg-center bg-cover '
         >
-            <div className='flex flex-col items-center justify-between h-full'>
+            <div className='flex flex-col items-center justify-between h-full font-montserrat'>
             <div className='pt-48 text-center'>
             <h1 className='text-4xl'>Model 3</h1>
             <p>Order Online for{""}  
@@ -17,10 +17,146 @@ const HomeScreen = () => {
                 </p>
             </div>
             {/* Button div */}
-            <div className=' flex text-center mb-24 gap-16'>
-                <button className='border-2 py-2 px-4 rounded-3xl text-sm'>Custom Order</button>
+            <div className=' flex text-center mb-24 gap-12'>
+                <button className='  bg-[#393c41] w-48 text-white py-1 px-4 rounded-3xl text-xs'>Custom Order</button>
 
-                <button className='border-2 py-2 px-4 rounded-3xl text-sm'>Demo Drive</button>
+                <button className=' w-48  text-[#393c41] bg-white py-1 px-4 rounded-3xl text-xs'>Demo Drive</button>
+            </div>
+            </div>
+        </div>
+
+
+        {/* Model Y */}
+        <div
+        style={{backgroundImage: 
+            'url("https://tesla-cdn.thron.com/delivery/public/image/tesla/81d6bbcd-e260-4176-86c4-cbd5033f9431/bvlatuR/std/1200x628/Model-Y-Social-EMEA-LHD?quality=auto-medium&format=auto")'
+         }}
+         className='flex flex-col h-screen item-center justify-between bg-center bg-cover font-montserrat'
+        >
+            <div className='flex flex-col items-center justify-between h-full'>
+            <div className='pt-48 text-center'>
+            <h1 className='text-4xl'>Model Y</h1>
+            <p>Order Online for{""}  
+                <span className='underline'> touchless delivery</span>
+                </p>
+            </div>
+            {/* Button div */}
+            <div className=' flex text-center mb-24 gap-12'>
+                <button className='  bg-[#393c41] w-48 text-white py-1 px-4 rounded-3xl text-xs'>Custom Order</button>
+
+                <button className=' w-48  text-[#393c41] bg-white py-1 px-4 rounded-3xl text-xs'>Demo Drive</button>
+            </div>
+            </div>
+        </div>
+         {/* Model S */}
+        <div
+        style={{backgroundImage: 
+            'url("https://tesla-cdn.thron.com/delivery/public/image/tesla/58949946-f4bf-4908-b707-18b2fa6020a5/bvlatuR/std/2880x1800/Homepage-Model-S-Desktop-LHD?quality=auto-medium&amp;format=auto")'
+         }}
+         className='flex flex-col h-screen item-center justify-between bg-center bg-cover font-montserrat'
+        >
+            <div className='flex flex-col items-center justify-between h-full'>
+            <div className='pt-48 text-center'>
+            <h1 className='text-4xl'>Model S</h1>
+            <p>Order Online for{""}  
+                <span className='underline'> touchless delivery</span>
+                </p>
+            </div>
+            {/* Button div */}
+            <div className=' flex text-center mb-24 gap-12'>
+                <button className='  bg-[#393c41] w-48 text-white py-1 px-4 rounded-3xl text-xs'>Custom Order</button>
+
+                <button className=' w-48  text-[#393c41] bg-white py-1 px-4 rounded-3xl text-xs'>Demo Drive</button>
+            </div>
+            </div>
+        </div>
+
+        {/* Model X */}
+        <div
+        style={{backgroundImage: 
+            'url("https://tesla-cdn.thron.com/delivery/public/image/tesla/a4fd80a1-095e-4ce1-8db1-95b3ff880c26/bvlatuR/std/2880x1800/Model-X-Main-Hero-Desktop-RHD?quality=auto-medium&amp;format=auto%C2%A0")'
+         }}
+         className='flex flex-col h-screen item-center justify-between bg-center bg-cover font-montserrat'
+        >
+            <div className='flex flex-col items-center justify-between h-full'>
+            <div className='pt-48 text-center'>
+            <h1 className='text-4xl'>Model X</h1>
+            <p>Order Online for{""}  
+                <span className='underline'> touchless delivery</span>
+                </p>
+            </div>
+            {/* Button div */}
+            <div className=' flex text-center mb-24 gap-12'>
+                <button className='  bg-[#393c41] w-48 text-white py-1 px-4 rounded-3xl text-xs'>Custom Order</button>
+
+                <button className=' w-48  text-[#393c41] bg-white py-1 px-4 rounded-3xl text-xs'>Demo Drive</button>
+            </div>
+            </div>
+        </div>
+
+        {/* Solar Panel */}
+        <div
+        style={{backgroundImage: 
+            'url("https://tesla-cdn.thron.com/delivery/public/image/tesla/45992f1c-a33a-4a04-b1f0-338aff182f8e/bvlatuR/std/2880x1800/_25-Hero-D")'
+         }}
+         className='flex flex-col h-screen item-center justify-between bg-center bg-cover font-montserrat'
+        >
+            <div className='flex flex-col items-center justify-between h-full'>
+            <div className='pt-48 text-center'>
+            <h1 className='text-4xl'>Solar Panels</h1>
+            <p>Order Online for{""}  
+                <span className='underline'> touchless delivery</span>
+                </p>
+            </div>
+            {/* Button div */}
+            <div className=' flex text-center mb-24 gap-12'>
+                <button className='  bg-[#393c41] w-48 text-white py-1 px-4 rounded-3xl text-xs'>Custom Order</button>
+
+                <button className=' w-48  text-[#393c41] bg-white py-1 px-4 rounded-3xl text-xs'>Demo Drive</button>
+            </div>
+            </div>
+        </div>
+        {/* Solar Roofs */}
+        <div
+        style={{backgroundImage: 
+            'url("https://tesla-cdn.thron.com/delivery/public/image/tesla/5299492b-ead4-4a79-a96e-02a030c0bbc4/bvlatuR/std/0x0/tesla_solar_roof")'
+         }}
+         className='flex flex-col h-screen item-center justify-between bg-center bg-cover font-montserrat'
+        >
+            <div className='flex flex-col items-center justify-between h-full'>
+            <div className='pt-48 text-center'>
+            <h1 className='text-4xl'>Solar Roofs</h1>
+            <p>Order Online for{""}  
+                <span className='underline'> touchless delivery</span>
+                </p>
+            </div>
+            {/* Button div */}
+            <div className=' flex text-center mb-24 gap-12'>
+                <button className='  bg-[#393c41] w-48 text-white py-1 px-4 rounded-3xl text-xs'>Custom Order</button>
+
+                <button className=' w-48  text-[#393c41] bg-white py-1 px-4 rounded-3xl text-xs'>Demo Drive</button>
+            </div>
+            </div>
+        </div>
+        {/* Accessories */}
+        <div
+        style={{backgroundImage: 
+            'url("https://www.teslarati.com/wp-content/uploads/2022/09/tesla-destination-charging-1-scaled.jpg")'
+         }}
+         className='flex flex-col h-screen item-center justify-between bg-center bg-cover font-montserrat'
+        >
+            <div className='flex flex-col items-center justify-between h-full'>
+            <div className='pt-48 text-center'>
+            <h1 className='text-4xl'>Accessories</h1>
+            <p>Order Online for{""}  
+                <span className='underline'> touchless delivery</span>
+                </p>
+            </div>
+            {/* Button div */}
+            <div className=' flex text-center mb-24 gap-12'>
+                <button className='  bg-[#393c41] w-48 text-white py-1 px-4 rounded-3xl text-xs'>Custom Order</button>
+
+                <button className=' w-48  text-[#393c41] bg-white py-1 px-4 rounded-3xl text-xs'>Demo Drive</button>
             </div>
             </div>
         </div>
